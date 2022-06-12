a1562be224759578841f8d57f6d5947d1be6ad9b
Olivier <olivier.halupczok@zwolnienizteorii.pl>
2022-06-12 21:22:58 +0200

    add sigmoid func

---
+++ Project/mathFuncs.cpp
@@ -0,0 +1,8 @@
+#include <math.h>
+
+#include "mathFuncs.h"
+
+double sigmoid(double arg) {
+    return 1 / (1+ std::exp(-arg));
+}
+
---
+++ Project/mathFuncs.h
@@ -0,0 +1,23 @@
+/**
+ * @file mathFuncs.h
+ * @author Olivier Halupczok
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef MATH_FUNCS_H
+#define MATH_FUNCS_H
+
+/**
+ * @brief return value of sigmoid function (1/(1 + exp(-arg)))
+ * 
+ * @param arg
+ * @return double 
+ */
+double sigmoid (double arg);
+ 
+
+#endif
