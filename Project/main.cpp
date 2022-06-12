9c7dda3a5ca402abb63534bc9466f20dd2c15fea
Olivier <olivier.halupczok@zwolnienizteorii.pl>
2022-06-12 21:23:48 +0200

    add compiling script, main.cpp and empty neuron.h

---
+++ Project/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author Olivier Halupczok
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <iterator>
+
+#include "mathFuncs.h"
+
+int main(int argc, char const *argv[])
+{
+    double test = sigmoid(0.12);
+    std::cout<<test;
+}
---
+++ Project/neuron.h
@@ -0,0 +1,23 @@
+/**
+ * @file neuron.h
+ * @author Olivier Halupczok
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef FLAG_H
+#define FLAG_H
+
+class Neuron
+{
+private:
+
+
+public:
+
+};
+
+#endif
